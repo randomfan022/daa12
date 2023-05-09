29c9b95a31acbe630515a85a90c1eff9ce69ecb6
randomfan022 <112581442+randomfan022@users.noreply.github.com>
2023-05-09 07:39:10 +0530

    Update p12.c

--- p12.c
+++ p12.c
@@ -1,49 +1,20 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-int paths[10][10]; 
-void Warshall (int n) 
-{ 
- int i,j,k; 
- for(k=0;k<n;k++) 
- { 
- for(i=0;i<n;i++) 
- { 
- for(j=0;j<n;j++) 
- { 
- paths[i][j]=(paths[i][j]) || (paths[i][k] && 
-paths[k][j]); 
- } 
- } 
- } 
-} 
-void main() 
-{ 
- int n,i,j,k; 
- clock_t start,end; 
- double clk; 
- 
- printf("ENTER NO OF CITIES\n"); 
- scanf("%d",&n); 
- 
- printf("ENTER MATRIX\n"); 
- for(i=0;i<n;i++) 
- for(j=0;j<n;j++) 
- scanf("%d",&paths[i][j]); 
- 
- start=clock(); 
- Warshall(n); 
- end=clock(); 
- 
- printf("TRANSITIVE CLOSURE MATRIX\n"); 
- for(i=0;i<n;i++) 
- { 
- for(j=0;j<n;j++) 
- printf("%d ",paths[i][j]); 
- printf("\n"); 
- } 
- 
- clk=(end-start)/CLOCKS_PER_SEC; 
- 
- printf("TIME REQUIRED IS %f\n",clk); 
-}
+df <- airquality
+
+dim(df)
+
+sapply(df, class)
+
+which(is.na(df))
+sum(is.na(df))
+
+df1 <- as.data.frame(df)
+
+for(i in 1:4)
+  df1[,i] <- ifelse(is.na(df1[,i]), mean(df1[,i],na.rm = TRUE), df1[,i])
+
+print(df1)
+
+df2 <- na.omit(df)
+
+print(df2)
+
